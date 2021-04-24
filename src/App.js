a3b69aa0cbf852f1441e1f302b0dbc78f65225b9
Marie Dasig <mariedasig@gmail.com>
2021-04-24 10:24:44 -0700

    added state to form component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import Todo from './components/Todo';
+import Form from './components/Form';
+import FilterButton from './components/FilterButton';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* eslint-disable jsx-a11y/no-redundant-roles */
+function App(props) {
+ const [tasks, setTasks] = useState(props.tasks);
+ function addTask(name) {
+  alert(name);
+ }
+ const taskList = tasks.map((task) => (
+  <Todo
+   id={task.id}
+   name={task.name}
+   completed={task.completed}
+   key={task.id}
+  />
+ ));
+ return (
+  <div className='todoapp stack-large'>
+   <h1>TodoMatic</h1>
+   <Form addTask={addTask} />
+   <div className='filters btn-group stack-exception'>
+    <FilterButton />
+    <FilterButton />
+    <FilterButton />
+   </div>
+   <h2 id='list-heading'>3 tasks remaining</h2>
+   <ul
+    role='list'
+    className='todo-list stack-large stack-exception'
+    aria-labelledby='list-heading'
+   >
+    {taskList}
+   </ul>
+  </div>
+ );
 }
 
 export default App;
